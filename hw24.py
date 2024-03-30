63921eef2ac5a3ef50a5b9cbfcc9f6cf9099fc9a
PetrKavan31 <petrkavan31@gmail.com>
2024-03-30 18:51:37 +0100

    total amount number of words in file without digits

--- hw24.py
+++ hw24.py
@@ -2,3 +2,8 @@
 with open('newTest.txt') as fr:
     data = fr.read()
     print(data)
+    lines = data.split()
+    nWords = 0
+    for words in lines:
+        if not words.isnumeric():
+            nWords += 1
